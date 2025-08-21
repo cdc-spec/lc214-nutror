2d8730daa07d0cfcd58eea87ef99a131f0e9d186
cdc-spec <caminhodocompetidor@gmail.com>
2025-08-21 19:12:55 -0300

    Update gerar-acesso.js

--- netlify/functions/gerar-acesso.js
+++ netlify/functions/gerar-acesso.js
@@ -2,18 +2,21 @@ const { getStore } = require("@netlify/blobs");
 const { randomUUID } = require("crypto");
 
 exports.handler = async (event, context) => {
-  // A URL base do site é obtida a partir do cabeçalho do evento
   const siteURL = new URL(event.rawUrl).origin;
   
-  const store = getStore("tokens_de_acesso");
-  const token = randomUUID();
-  const expiration = Date.now() + 2 * 60 * 1000; // 2 minutos em milissegundos
+  // Passando as credenciais manualmente, como sugerido pelo erro
+  const store = getStore({
+    name: "tokens_de_acesso",
+    siteID: process.env.SITE_ID,
+    token: process.env.NETLIFY_API_TOKEN,
+  });
   
+  const token = randomUUID();
+  const expiration = Date.now() + 2 * 60 * 1000;
   await store.setJSON(token, { expires: expiration });
   
   const redirectURL = `${siteURL}/?token=${token}`;
   
-  // Retorna uma resposta de redirecionamento
   return {
     statusCode: 302,
     headers: {
